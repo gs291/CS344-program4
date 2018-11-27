0b9cb6a6734d0dd80a07397bf5a6f2b294b6bd77
gs291 <sanchez.gregory886@gmail.com>
2018-11-27 02:10:27 -0800

    Update decoder

--- otp_dec.c
+++ otp_dec.c
@@ -1,3 +1,101 @@
-void main () {
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
 
+void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
+
+int checkFileContents (const char *file, char *buffer) {
+  int i = 0;
+  FILE *fp;
+
+  fp = fopen(file, "r");
+
+  if(fp == NULL) {
+    printf("Unable to open %s\n", file);
+    perror("In checkFileContents\n");
+    exit(1);
+  }
+  	memset(buffer, '\0', sizeof(buffer));
+    if ( fgets(buffer, 1024, fp) !=NULL ) {
+      buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+    }
+    fclose(fp);
+
+    while(buffer[i] != '\0') {
+      if(buffer[i] > 90 || (buffer[i] < 65 && buffer[i] != 32))
+        return 0;
+      i++;
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	int socketFD, portNumber, charsWritten, charsRead;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+	char buffer[1024], sendingMsg[2048];
+
+	if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
+
+	// Set up the server address struct
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+
+	// Set up the socket
+	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (socketFD < 0) error("CLIENT: ERROR opening socket");
+
+	// Connect to server
+	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+		error("CLIENT: ERROR connecting");
+
+	// Get input message from user
+	// printf("CLIENT: Enter text to send to the server, and then hit enter: ");
+	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
+  memset(sendingMsg, '\0', sizeof(sendingMsg)); // Clear out the sendingMsg array
+  if(checkFileContents(argv[1], buffer) == 0) {
+    fprintf(stderr, "otp_enc error: input contains bad characters\n");
+    exit(1);
+  }
+  strcpy(sendingMsg, "@");
+  strcat(sendingMsg, buffer);
+  memset(buffer, '\0', sizeof(buffer));
+  if(checkFileContents(argv[2], buffer) == 0) {
+    fprintf(stderr, "otp_enc error: input contains bad characters\n");
+    exit(1);
+  }
+  strcat(sendingMsg, "$");
+  strcat(sendingMsg, buffer);
+	// Send message to server
+	charsWritten = send(socketFD, sendingMsg, strlen(sendingMsg), 0); // Write to the server
+	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+	if (charsWritten < strlen(sendingMsg)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+	// Get return message from server
+	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
+	charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
+	if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+
+  if(strcmp(buffer, "!key") == 0) {
+    fprintf(stderr, "ERROR: key '%s' is too short\n", argv[2]);
+  } else if (strcmp(buffer, "!con") == 0) {
+    fprintf(stderr, "ERROR: could not contact otp_enc_d on port %d\n", portNumber);
+  } else {
+    printf("%s\n", buffer);
+  }
+
+	close(socketFD); // Close the socket
+	return 0;
 }
--- otp_dec_d.c
+++ otp_dec_d.c
@@ -1,3 +1,132 @@
-void main () {
-  
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int ciphLen = 0, keyLen = 0;
+char cipher[1024], key[1024];
+
+int parseMsg(char *msg) {
+	int i = 1;
+	memset(cipher, '\0', 1024);
+	memset(key, '\0', 1024);
+
+	if(msg[0] == '@') {
+		while(msg[i] != '$') {
+			cipher[ciphLen] = msg[i];
+			i++; ciphLen++;
+		}
+		if(msg[i] == '$') {
+			i++;
+			while(msg[i] != '\0') {
+				key[keyLen] = msg[i];
+				i++; keyLen++;
+			}
+		} else
+			return 0;
+	} else
+		return 0;
+
+	return 1;
+}
+
+int decText (char *encMsg) {
+	int i, cT, cK;
+
+  memset(encMsg, '\0', 1024);
+	if(keyLen >= ciphLen) {
+			for (i = 0; i < ciphLen; i++) {
+				cT = cipher[i] - 65; cK = key[i] - 65;
+				if(cT < 0) { cT = 26; } if (cK < 0) { cK = 26; }
+				int cMsg = (cT - cK);
+        if(cMsg < 0) { cMsg+= 27; }
+        cMsg = cMsg % 27 + 65;
+				if(cMsg == 91) {cMsg = 32;}
+				encMsg[i] = cMsg;
+			}
+	} else
+		return 0;
+
+		return 1;
+}
+
+void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+
+void sendMsg(int connectionFD, const char *msg, int length) {
+		// Send a Success message back to the client;
+	int charsRead = send(connectionFD, msg, length, 0); // Send success back
+	if (charsRead < 0) error("ERROR writing to socket");
+}
+
+int main(int argc, char *argv[])
+{
+	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, childExitStatus = -5;;
+	pid_t spawnPid = -5;
+	socklen_t sizeOfClientInfo;
+	char buffer[2048], encMsg[1024];
+	struct sockaddr_in serverAddress, clientAddress;
+
+	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+
+	// Set up the address struct for this process (the server)
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
+
+	// Set up the socket
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (listenSocketFD < 0) error("ERROR opening socket");
+
+	// Enable the socket to begin listening
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+		error("ERROR on binding");
+	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+
+  while(1) {
+  	// Accept a connection, blocking if one is not available until one connects
+  	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+  	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+  	if (establishedConnectionFD < 0) error("ERROR on accept");
+
+  	// Get the message from the client and display it
+  	memset(buffer, '\0', 2048);
+  	charsRead = recv(establishedConnectionFD, buffer, 2048, 0); // Read the client's message from the socket
+  	if (charsRead < 0) error("ERROR reading from socket");
+
+  	spawnPid = fork();
+  	switch (spawnPid) {
+  	      case -1: {
+  	        perror("Hull Breach!\n"); exit(1); break;
+  	      }
+  	    case 0: {  /* CHILD PROCESS */
+  	        if(parseMsg(buffer) == 1) {
+  						if(decText(encMsg) == 1) {
+  							sendMsg(establishedConnectionFD, encMsg, ciphLen);
+  							exit(0);
+  						} else {
+  							sendMsg(establishedConnectionFD, "!key", 4);
+  							exit(2);
+  						}
+  					} else {
+  						sendMsg(establishedConnectionFD, "!con", 4);
+  		      	exit(2);
+  					}
+  					 break;
+  	    }
+  	    default: {  /* PARENT PROCESS */
+  				pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0); /* Waits until the child completes */
+  	      break;
+  	    }
+  	  }
+
+
+  	close(establishedConnectionFD); // Close the existing socket which is connected to the client
+  }
+  close(listenSocketFD); // Close the listening socket
+	return 0;
 }
